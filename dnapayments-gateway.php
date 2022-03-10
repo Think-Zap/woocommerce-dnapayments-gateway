d12ab45bac984172818b18242a569b34f9e18778
Nurbol Sarsenbayev <nurbol@dnapayments.com>
2022-03-10 11:25:29 +0600

    update version of plugin

--- dnapayments-gateway.php
+++ dnapayments-gateway.php
@@ -11,7 +11,7 @@
 
 define( 'WC_DNA_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'WC_DNA_MAIN_FILE', __FILE__ );
-define( 'WC_DNA_VERSION', '1.1.2' );
+define( 'WC_DNA_VERSION', '1.1.3' );
 define( 'WC_DNA_MIN_PHP_VER', '5.6.0' );
 define( 'WC_DNA_MIN_WC_VER', '3.0' );
 
